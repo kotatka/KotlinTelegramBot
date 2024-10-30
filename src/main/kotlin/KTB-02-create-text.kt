2c2ab29f37f239a223d9285788453dc0b91cc7c7
kotatka <tyrrrr5665@gmail.com>
2024-10-30 12:08:28 +0300

    KTB-02-create-text

--- src/main/kotlin/KTB-02-create-text.kt
+++ src/main/kotlin/KTB-02-create-text.kt
@@ -2,9 +2,5 @@ import java.io.File
 
 fun main() {
     val wordsFile: File = File("words.txt")
-    wordsFile.createNewFile()
-    val wordsStrings = wordsFile.readLines()
-    for (i in 0..wordsStrings.size - 1) {
-        println(wordsStrings[i])
-    }
+    wordsFile.forEachLine { line -> println(line) }
 }
