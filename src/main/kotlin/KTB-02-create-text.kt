880c7e3de673b8ed2c5adce930c7902417b2d95e
kotatka <tyrrrr5665@gmail.com>
2024-10-31 11:08:57 +0300

    KTB-04-create-text

--- src/main/kotlin/KTB-02-create-text.kt
+++ src/main/kotlin/KTB-02-create-text.kt
@@ -1,6 +1,24 @@
 import java.io.File
 
+data class Word(
+    val original: String,
+    val translate: String,
+    var correctAnswersCount: Int?,
+)
+
 fun main() {
     val wordsFile: File = File("words.txt")
-    wordsFile.forEachLine { line -> println(line) }
+    val dictionary: MutableList<Word> = mutableListOf()
+
+    val lines: List<String> = wordsFile.readLines()
+    for (line in lines) {
+        val wordsLine = line.split("|")
+        val newWord = Word(
+            original = wordsLine[0],
+            translate = wordsLine[1],
+            correctAnswersCount = wordsLine[2].toIntOrNull() ?: 0
+        )
+        dictionary.add(newWord)
+    }
+    println(dictionary)
 }
